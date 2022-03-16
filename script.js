ac44e6bcea96de03181c125c7c47f15e8d8cd23a
cleosch <cleojschurink@gmail.com>
2022-03-16 15:35:53 +1300

    Update script.js

--- script.js
+++ script.js
@@ -2,89 +2,83 @@
 import { gpx } from "https://unpkg.com/@tmcw/togeojson?module";
 
 // Importing ArcGIS API bits and bobs
-import EsriMap from "esri/Map.js";
-import SceneView from "esri/views/SceneView.js";
-import ElevationProfile from "esri/widgets/ElevationProfile.js";
-import { LineSymbol3D, LineSymbol3DLayer, PointSymbol3D, IconSymbol3DLayer } from "esri/symbols.js";
-import { Polyline, Point } from "esri/geometry.js";
-import ElevationProfileLineInput from "esri/widgets/ElevationProfile/ElevationProfileLineInput.js";
-import Graphic from "esri/Graphic.js";
-import GraphicsLayer from "esri/layers/GraphicsLayer.js";
+import ArcGISWebScene from '@arcgis/core/WebScene';
+import SceneView from '@arcgis/core/views/SceneView';
+import ArcGISMap from '@arcgis/core/Map';
+import MapView from '@arcgis/core/views/MapView';
+import request from '@arcgis/core/request';
+import ElevationProfile from "@arcgis/core/widgets/ElevationProfile";
+import Basemap from "@arcgis/core/Basemap";
+import Polyline from "@arcgis/core/geometry/Polyline";
+import Graphic from "@arcgis/core/Graphic";
+import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
+import VectorTileLayer from "@arcgis/core/layers/VectorTileLayer";
+import TileLayer from "@arcgis/core/layers/TileLayer";
+import LabelClass from "@arcgis/core/layers/support/LabelClass";
+import { tcx } from "@tmcw/togeojson";
+import "./index.css";
 
-const map = new EsriMap({
-  basemap: "satellite",
-  ground: "world-elevation",
+const lightBlue = [129, 175, 214]
+const lightBrown = [161, 136, 119];
+const orange = [245, 173, 66];
+
+const miniMap = new ArcGISMap({
+  basemap: new Basemap({
+    baseLayers: [
+      new TileLayer({
+        portalItem: {
+          id: "1b243539f4514b6ba35e7d995890db1d" // world hillshade
+        },
+        opacity: 0.4
+      }),
+      new VectorTileLayer({
+        portalItem:{
+          id: "378fd91096fe478cb78a4e06b639b715"
+        },
+        blendMode: "multiply"
+     })
+    ]
+  })
 });
 
-const view = new SceneView({
-  map: map,
-  container: "viewDiv",
-  qualityProfile: "high",
-  camera: {
-    position: [
-      168.95148337,
-      -45.02154658,
-      15161.47986
-    ],
-    heading: 310.62,
-    tilt: 57.89
-  },
-  environment: {
-    atmosphere: { quality: "high" },
-  },
+new MapView({
+  container: "miniMap",
+  map: miniMap,
+  center: [172.68010648, -42.47741997],
+  zoom: 6,
   ui: {
-    components: ["navigation-toggle"],
-  },
-  popup: {
-    defaultPopupTemplateEnabled: true
+    components: []
   }
 });
 
-const elevationProfile = new ElevationProfile({
-  view,
-  profiles: [
-    new ElevationProfileLineInput({ color: [212, 42, 56], title: "Coronet Loop" }),
-  ],
-  visibleElements: {
-    selectButton: false,
-    sketchButton: false,
-    settingsButton: false,
+const citiesLayer = new FeatureLayer({
+  url: "https://services8.arcgis.com/AYGZtmUtpARUKBlB/arcgis/rest/services/Te_Reo_M%C4%81ori_Place_Names/FeatureServer",
+  definitionExpression: "name IN ('Christchurch', 'Kaikoura', 'Greymouth', 'Hokitika', 'Punakaiki')",
+  renderer: {
+    type: "unique-value",
+    field: "name_mi",
+    defaultSymbol: {
+      type: "simple-marker",  // autocasts as new SimpleMarkerSymbol()
+      style: "circle",
+      color: [150, 150, 150],
+      size: "8px",  // pixels
+      outline: {
+        width: 0  // points
+      }
+    },
   },
+  labelingInfo: [
+    new LabelClass({
+      labelExpressionInfo: { expression: "$feature.name_mi"},
+      labelPlacement: "above-center",
+      symbol: {
+        type: "text",
+        color: [100, 100, 100],
+        haloSize: 1,
+        haloColor: [255, 255, 255, 0.9]
+      }
+    })
+  ]
 });
 
-view.ui.add(elevationProfile, "top-right");
-
-(async () => {
-  // read the gpx file and convert it to geojson
-  const response = await fetch("./cycling.gpx");
-  const gpxcontent = await response.text();
-  const geojson = gpx(new DOMParser().parseFromString(gpxcontent, "text/xml"));
-  const coordinates = geojson.features[0].geometry.coordinates;
-
-  // add the track as an input for the ElevationProfile widget
-  const geometry = new Polyline({
-    paths: [coordinates],
-    hasZ: true
-  });
-  elevationProfile.input = new Graphic({ geometry: geometry });
-
-  // add the bike track layer as a graphics layer - like a template
-  const bikeTrackLayer = new GraphicsLayer({
-    elevationInfo: {
-      mode: "on-the-ground"
-    },
-    listMode: "hide"
-  });
-
-  const bikeTrack = new Graphic({
-    geometry: geometry,
-    symbol: new LineSymbol3D({
-      symbolLayers: [new LineSymbol3DLayer({
-        material: { color: [212, 42, 56] },
-        size: 3
-      })]
-    })
-  });
-  bikeTrackLayer.add(bikeTrack);
-  map.add(bikeTrackLayer);
-})();
+miniMap.add(citiesLayer);
