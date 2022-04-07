ea6d7bb2fdcf75da0471843d9f6e6a3308ef6177
cleosch <cleojschurink@gmail.com>
2022-04-07 15:08:21 +1200

    Big boi revamp

--- script.js
+++ script.js
@@ -2,152 +2,146 @@
 import { gpx } from "https://unpkg.com/@tmcw/togeojson?module";
 
 // Importing ArcGIS API bits and bobs
-import EsriMap from "esri/Map.js";
-import SceneView from "esri/views/SceneView.js";
-import ElevationProfile from "esri/widgets/ElevationProfile.js";
-import { LineSymbol3D, LineSymbol3DLayer, PointSymbol3D, IconSymbol3DLayer } from "esri/symbols.js";
-import { Polyline, Point } from "esri/geometry.js";
-import ElevationProfileLineInput from "esri/widgets/ElevationProfile/ElevationProfileLineInput.js";
-import Graphic from "esri/Graphic.js";
-import GraphicsLayer from "esri/layers/GraphicsLayer.js";
+require(["esri/views/SceneView", "esri/Map"], function (SceneView, Map) {
 
-const map = new EsriMap({
+  var map_pls = new Map({
   basemap: "satellite",
   ground: "world-elevation",
 });
-
-const view = new SceneView({
-  map: map,
-  container: "viewDiv",
-  qualityProfile: "high",
-  camera: {
-    position: [
-      171.50188356,
-      -42.24462024,
-      109734.53680
+ 
+  var view = new SceneView({
+    map: map_pls,
+    container: "map", 
+    camera: {
+   position: [
+      168.98609855,
+      -45.01302070,
+      14872.53988
     ],
-    heading: 358.85,
-    tilt: 0.49
+    heading: 304.74,
+    tilt: 52.43
   },
-  environment: {
-    atmosphere: { quality: "high" },
-  },
-  ui: {
-    components: ["navigation-toggle"],
-  },
-  popup: {
-    defaultPopupTemplateEnabled: true
-  }
-});
-
-const elevationProfile = new ElevationProfile({
-  view,
-  container: "profile",
-  profiles: [
-    new ElevationProfileLineInput({ color: [212, 42, 56], title: "Paparoa Track" }),
-  ],
-  visibleElements: {
-    selectButton: false,
-    sketchButton: false,
-    settingsButton: false,
-  },
-});
-
-(async () => {
-  // read the gpx file and convert it to geojson
-  const response = await fetch("./paparoa.gpx");
-  const gpxcontent = await response.text();
-  const geojson = gpx(new DOMParser().parseFromString(gpxcontent, "text/xml"));
-  const coordinates = geojson.features[0].geometry.coordinates;
-
-  // add the track as an input for the ElevationProfile widget
-  const geometry = new Polyline({
-    paths: [coordinates],
-    hasZ: true
   });
-  elevationProfile.input = new Graphic({ geometry: geometry });
-
-  // add the bike track layer as a graphics layer - like a template
-  const bikeTrackLayer = new GraphicsLayer({
-    elevationInfo: {
-      mode: "on-the-ground"
-    },
-    listMode: "hide"
-  });
-
-  const bikeTrack = new Graphic({
-    geometry: geometry,
-    symbol: new LineSymbol3D({
-      symbolLayers: [new LineSymbol3DLayer({
-        material: { color: [212, 42, 56] },
-        size: 3
-      })]
-    })
-  });
-  bikeTrackLayer.add(bikeTrack);
-  map.add(bikeTrackLayer);
-})();
-
-const inset_map = new EsriMap({
-  basemap: "streets-relief-vector",
+  
+          function catchAbortError(error) {
+          if (error.name != "AbortError") {
+            console.error(error);
+          }
+        }
+  
+  document.getElementById("macetown").addEventListener("click", () => {
+          view
+            .goTo(
+              {
+                position: {
+                  x: 168.87633904,
+                  y: -44.83948228,
+                  z: 5177.04182,
+                  spatialReference: {
+                    wkid: 4326
+                  }
+                },
+                heading: 200.35,
+                tilt: 55.19
+              },
+            )
+            .catch(catchAbortError);
+        });
+  
+  document.getElementById("back").addEventListener("click", () => {
+          view
+            .goTo(
+              {
+                position: {
+                  x: 168.76724954,
+                  y: -44.85663036,
+                  z: 6393.75519,
+                  spatialReference: {
+                    wkid: 4326
+                  }
+                },
+                heading: 146.36,
+                tilt: 43.25
+              },
+            )
+            .catch(catchAbortError);
+        });
+  
+  document.getElementById("skippers").addEventListener("click", () => {
+          view
+            .goTo(
+              {
+                position: {
+                  x: 168.55147406,
+                  y: -44.88119739,
+                  z: 8133.16600,
+                  spatialReference: {
+                    wkid: 4326
+                  }
+                },
+                heading: 114.64,
+                tilt: 56.49
+              },
+            )
+            .catch(catchAbortError);
+        });
+    
+  document.getElementById("coronet").addEventListener("click", () => {
+          view
+            .goTo(
+              {
+                position: {
+                  x: 168.73441229,
+                  y: -44.99226340,
+                  z: 5620.77873,
+                  spatialReference: {
+                    wkid: 4326
+                  }
+                },
+                heading: 2.07,
+                tilt: 58.51
+              },
+            )
+            .catch(catchAbortError);
+        });
+  
+  
+   document.getElementById("bush_gully").addEventListener("click", () => {
+          view
+            .goTo(
+              {
+                position: {
+                  x: 168.72293973,
+                  y: -44.93566277,
+                  z: 2880.32082,
+                  spatialReference: {
+                    wkid: 4326
+                  }
+                },
+                heading: 84.36,
+                tilt: 65.51
+              },
+            )
+            .catch(catchAbortError);
+        });
+  
+   document.getElementById("arrowtown").addEventListener("click", () => {
+          view
+            .goTo(
+              {
+                position: {
+                  x: 168.84206698,
+                  y: -44.96918298,
+                  z: 1961.40883,
+                  spatialReference: {
+                    wkid: 4326
+                  }
+                },
+                heading: 341.10,
+                tilt: 67.85
+              },
+            )
+            .catch(catchAbortError);
+        });
+  
 });
-
-new MapView({
-  map: inset_map,
-  container: "miniMap",
-  center: [172.44994752,
-    -42.46085216],
-  zoom: 5,
-  ui: {
-    components: []
-  }
-});
-
-const viewpoints = {
-  christchurch: {
-    position: [
-      7.66371179,
-      46.50680737,
-      1944.91065
-    ],
-    heading: 153.80,
-    tilt: 66.63
-  },
-  punakaiki: {
-    position: [
-      7.66950766,
-      46.48476542,
-      1570.19567
-    ],
-    heading: 207.12,
-    tilt: 75.00
-  },
-  porarari: {
-    position: [
-      7.65150784,
-      46.45879114,
-      1954.86814
-    ],
-    heading: 219.39,
-    tilt: 77.12
-  },
-  moonlight: {
-    position: [
-      7.63867205,
-      46.44178047,
-      2829.83936
-    ],
-    heading: 221.31,
-    tilt: 66.06
-  }
-}
-
-
-const viewpointBtns = document.getElementsByClassName("viewpointBtn");
-
-for (let i = 0; i < viewpointBtns.length; i++) {
-  const elem = viewpointBtns.item(i);
-  elem.addEventListener("click", function(event) {
-    view.goTo(viewpoints[event.target.dataset.value]);
-  });
-}
