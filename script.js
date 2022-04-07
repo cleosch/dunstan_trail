3e90c7ed6cfe43d1d74e6048357fafd6ce33a870
cleosch <cleojschurink@gmail.com>
2022-04-08 09:35:56 +1200

    Update script.js

--- script.js
+++ script.js
@@ -88,4 +88,119 @@ const bikeTrack = new Graphic({
 
 bikeTrackLayer.add(bikeTrack);
 map.add(bikeTrackLayer);
+})();
+
+function catchAbortError(error) {
+  if (error.name != "AbortError") {
+    console.error(error);
+  }
+}
+  
+document.getElementById("macetown").addEventListener("click", () => {
+  view.goTo({
+    position: {
+      x: 168.87633904,
+      y: -44.83948228,
+      z: 5177.04182,
+      spatialReference: {
+          wkid: 4326
+        }
+      },
+    heading: 200.35,
+    tilt: 55.19
+  },
+  )
+  .catch(catchAbortError);
+});
+  
+document.getElementById("back").addEventListener("click", () => {
+  view.goTo({
+    position: {
+      x: 168.76724954,
+      y: -44.85663036,
+      z: 6393.75519,
+      spatialReference: {
+        wkid: 4326
+        }
+      },
+      heading: 146.36,
+      tilt: 43.25
+    },
+    )
+    .catch(catchAbortError);
+  });
+  
+document.getElementById("skippers").addEventListener("click", () => {
+  view.goTo({
+    position: {
+      x: 168.55147406,
+      y: -44.88119739,
+      z: 8133.16600,
+      spatialReference: {
+        wkid: 4326
+      }
+    },
+    heading: 114.64,
+    tilt: 56.49
+  },
+)
+.catch(catchAbortError);
+});
+    
+document.getElementById("coronet").addEventListener("click", () => {
+  view
+    .goTo(
+      {
+        position: {
+          x: 168.73441229,
+          y: -44.99226340,
+          z: 5620.77873,
+          spatialReference: {
+            wkid: 4326
+          }
+        },
+        heading: 2.07,
+        tilt: 58.51
+      },
+    )
+    .catch(catchAbortError);
+});
+  
+  
+document.getElementById("bush_gully").addEventListener("click", () => {
+  view
+    .goTo(
+      {
+        position: {
+          x: 168.72293973,
+          y: -44.93566277,
+          z: 2880.32082,
+          spatialReference: {
+            wkid: 4326
+          }
+        },
+        heading: 84.36,
+        tilt: 65.51
+      },
+    )
+    .catch(catchAbortError);
+});
+  
+document.getElementById("arrowtown").addEventListener("click", () => {
+  view
+    .goTo(
+      {
+        position: {
+          x: 168.84206698,
+          y: -44.96918298,
+          z: 1961.40883,
+          spatialReference: {
+            wkid: 4326
+          }
+        },
+        heading: 341.10,
+        tilt: 67.85
+      },
+    )
+    .catch(catchAbortError);
 });
